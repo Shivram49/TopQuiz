f25877f50d1fcd388b1884d400c85702c5d8295c
Shivram49 <shivr4015@gmail.com>
2023-03-14 14:13:04 -0700

    Initial commit - baseline changes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ooad.assign.topquiz {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ooad.assign.topquiz to javafx.fxml;
+    exports ooad.assign.topquiz;
+}
