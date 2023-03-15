5bb795684b2ed2abcec314fd5fe478a61140b406
Shivram49 <shivr4015@gmail.com>
2023-03-14 19:36:52 -0700

    adding questions.java, Student.java, dao folder

---
+++ src/main/java/ooad/assign/topquiz/models/Person.java
@@ -0,0 +1,38 @@
+package ooad.assign.topquiz.models;
+
+import ooad.assign.topquiz.services.PersonInterface;
+
+public class Person implements PersonInterface {
+    private int name;
+
+    public Person() {
+    }
+
+    public Person(int name) {
+        this.name = name;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        //username password should be validated with database
+        return true;
+    }
+
+    @Override
+    public boolean logout() {
+        return false;
+    }
+
+    @Override
+    public Score displayPerformance() {
+        return null;
+    }
+}
