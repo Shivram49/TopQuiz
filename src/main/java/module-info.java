79356754dd643395adeeed5c3a0e071e3327c77b
Shivram49 <shivr4015@gmail.com>
2023-03-16 20:04:06 -0700

    complete code - without latest performance

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,10 @@ module ooad.assign.topquiz {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens ooad.assign.topquiz to javafx.fxml;
     exports ooad.assign.topquiz;
+    exports ooad.assign.topquiz.controllers;
+    opens ooad.assign.topquiz.controllers to javafx.fxml;
 }
